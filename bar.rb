37e7d152f16b2a0396e85a92276e4c6bc8df8c44
Bert Overduin <boverduin@gmail.com>
2018-02-12 10:18:14 +0000

    Add few improvements after homework review

--- bar.rb
+++ bar.rb
@@ -19,32 +19,47 @@ class Bar
     @tabs << {guest: guest, drinks: []}
   end
 
+  # def add_to_tab(guest, drink)
+  #   for tab in tabs
+  #     tab[:drinks] << drink if tab[:guest] == guest
+  #   end
+  # end
+
   def add_to_tab(guest, drink)
-    for tab in tabs
-      tab[:drinks] << drink if tab[:guest] == guest
-    end
+    tab = @tabs.find {|x| x[:guest] == guest}
+    tab[:drinks] << drink
   end
 
+  # def tab_total(guest)
+  #   tab_total = 0.00
+  #   for tab in @tabs
+  #     if tab[:guest] == guest
+  #       for drink in tab[:drinks]
+  #         tab_total += drink.price()
+  #       end
+  #     end
+  #   end
+  #   return tab_total
+  # end
+
   def tab_total(guest)
-    tab_total = 0.00
-    for tab in @tabs
-      if tab[:guest] == guest
-        for drink in tab[:drinks]
-          tab_total += drink.price()
-        end
-      end
-    end
-    tab_total
+    tab = @tabs.find {|x| x[:guest] == guest}
+    tab_total = tab[:drinks].sum {|x| x.price()}
+    return tab_total
   end
 
   def get_payment(payment)
     @till += payment
   end
 
+  # def close_tab(guest)
+  #   for tab in @tabs
+  #     @tabs.delete(tab) if tab[:guest] == guest
+  #   end
+  # end
+
   def close_tab(guest)
-    for tab in @tabs
-      @tabs.delete(tab) if tab[:guest] == guest
-    end
+    @tabs.delete_if {|tab| tab[:guest] == guest}
   end
 
 end
